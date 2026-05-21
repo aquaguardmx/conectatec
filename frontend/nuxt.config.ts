d25d022046952e925d3c1a505d3b259745d6822d
GerardoPz <pliegolugohectorgerardo@gmail.com>
2026-05-21 17:53:57 -0600

    ya xcc

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,23 +1,19 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15', 
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  // 1. Agregamos la configuración de los iconos aquí
+  css: ['~/assets/css/main.css'],
+
   app: {
     head: {
       link: [
-        { 
-          rel: 'stylesheet', 
-          href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' 
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'
         }
       ]
     }
   },
-
-  // Tus módulos existentes
-  modules: [
-  '@nuxtjs/tailwindcss'
-  ],
 
   vite: {
     optimizeDeps: {
