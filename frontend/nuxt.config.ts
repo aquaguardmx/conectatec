76fdbef910133aa73ee0785b9a712aeaf19eb6d3
GerardoPz <pliegolugohectorgerardo@gmail.com>
2026-05-21 17:59:31 -0600

    s

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  css: ['~/assets/css/main.css'],
+  css: ['~/app/assets/css/main.css'],
 
   app: {
     head: {
